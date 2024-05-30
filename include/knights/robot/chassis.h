fa5b51d54764b51e67cd0e07d4ad235bfd1b0e22
NRGKing <nrgninjaking@gmail.com>
2024-05-29 23:12:22 -0400

    docs: added commenting to the chassis file

--- include/knights/robot/chassis.h
+++ include/knights/robot/chassis.h
@@ -30,43 +30,76 @@ namespace knights {
             // declare the robot chassis class as a friend class, allows access into private objects
             friend class Robot_Controller;
         public:
-            // @brief create a robot chassis with provided parameters
-            // @param drivetrain the drivetrain to use for the chassis
-            // @param pos_trackers the sensors to use for location tracking
+            /**
+             * @brief Construct a new Robot Chassis object
+             * 
+             * @param drivetrain a pointer to the drivetrain to use for the chassis
+             * @param pos_trackers a pointer to the sensors to use for location tracking
+             */
             Robot_Chassis(Drivetrain *drivetrain, Position_Tracker_Group *pos_trackers);
 
-            // @brief create a robot chassis with provided parameters
-            // @param drivetrain the drivetrain to use for the chassis
-            // @param pos_trackers the sensors to use for location tracking
+
+            /**
+             * @brief Construct a new Robot Chassis object
+             * 
+             * @param drivetrain a pointer to the drivetrain to use for the chassis
+             * @param pos_trackers a pointer to the sensors to use for location tracking
+             */
             Robot_Chassis(Holonomic *drivetrain, Position_Tracker_Group *pos_trackers);
 
-            // @brief set the position of the chassis
-            // @param x the desired x
-            // @param y the desired y
-            // @param heading the desired heading
+            /**
+             * @brief Set the position of the chassis
+             * 
+             * @param x desired X value
+             * @param y desired Y value
+             * @param heading desired heading value (in radians)
+             */
             void set_position(float x, float y, float heading);
 
-            // @brief set the position of the chassis
-            // @param position the position to set it to
+            /**
+             * @brief Set the position of the chassis
+             * 
+             * @param position a position variable containing an x, y, and heading
+             */
             void set_position(Pos position);
 
-            // @brief update the position of a chassis using its tracking system
+            /**
+             * @brief Update the position of the chassis using its tracking method.
+             *  
+             *  Will not update it no tracking method is set up.
+             * 
+             */
             void update_position();
 
-            // @brief Get the current position of the chassis
+            /**
+             * @brief Get the current position of the chassis
+             * 
+             * @return Pos The current position of the chassis
+             */
             Pos get_position();
 
-            // @brief set the position of the chassis
-            // @param x the desired x
-            // @param y the desired y
-            // @param heading the desired heading
+
+            /**
+             * @brief Set the previous position of the chassis
+             * 
+             * @param x desired X value
+             * @param y desired Y value
+             * @param heading desired heading value (in radians)
+             */
             void set_prev_position(float x, float y, float heading);
 
-            // @brief set the position of the chassis
-            // @param position the position to set it to
+            /**
+             * @brief Set the previous position of the chassis
+             * 
+             * @param position a position variable containing an x, y, and heading
+             */
             void set_prev_position(Pos position);
 
-            // @brief Get the current position of the chassis
+            /**
+             * @brief Get the prev position of the chassis
+             * 
+             * @return Pos the previous position of the chassis
+             */
             Pos get_prev_position();
 
     };
