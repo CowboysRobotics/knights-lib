52da71e6f4f983a9ae3ea770dd664e39138914d1
Student <cwbys8568@gmail.com>
2024-06-04 15:22:28 -0400

    feature: blueprint for profile generator and profile timestamp finished

--- include/knights/autonomous/profile.h
+++ include/knights/autonomous/profile.h
@@ -1,13 +1,34 @@
 #pragma once
 
+#include "knights/util/position.h"
 #ifndef _PROFILE_H
 #define _PROFILE_H
 
+#include "knights/robot/drivetrain.h"
+
 namespace knights {
 
-    class Profile_Generator {
+    struct ProfileTimestamp {
+        knights::Pos position;
+        float expected_velocity;
+        float time;
+        float right_speed;
+        float left_speed;
+    };
+
+    class ProfileGenerator {
         private:
-            
+            float rpm = 0;
+            float wheel_diameter = 0;
+            knights::Drivetrain *chassis = nullptr;
+        public:
+            float speed = 0;
+
+            ProfileGenerator(knights::Drivetrain);
+
+            ProfileGenerator(float rpm, float wheel_diameter);
+
+            std::vector<ProfileTimestamp> generate_profile();
     };
 
 }
