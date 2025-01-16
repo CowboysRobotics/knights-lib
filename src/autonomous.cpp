b6bf63b398d37b06a4760fc47465400e60ccbe29
Student <cwbys8568@gmail.com>
2025-01-15 19:26:27 -0500

    tried to integrate lemlib static folder - not working, probably delete

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,4 +1,5 @@
 #include "autonomous.h" 
+#include "hold-cold-asset/asset.hpp"
 
 #define RIGHT 1
 #define LEFT -1
@@ -7,9 +8,23 @@
 #define LATERAL_kI 0
 #define LATERAL_kD 0.0065
 
-#define TURN_kP 54
-#define TURN_kI 0.017
-#define TURN_kD 0.002
+#define TURN_kP_45 75
+#define TURN_kI_45 0.017
+#define TURN_kD_45 0.08
+
+#define TURN_kP_90 48 // 75 - 45 // 48 - 90 // 38 - 135 // 34 - 180
+#define TURN_kI_90 0.017 // 0.017 - 45 // 0.017 - 90 // 0.017 - 135 // 0.017 - 180
+#define TURN_kD_90 0.24 // 0.08 - 45 // 0.24 - 90 // 0.24 - 135 // 0.24 - 180
+
+#define TURN_kP_135 38
+#define TURN_kI_135 0.017
+#define TURN_kD_135 0.24
+
+#define TURN_kP_180 34
+#define TURN_kI_180 0.017
+#define TURN_kD_180 0.24
+
+ASSET(output_txt);
 
 void pid_tuning(knights::RobotChassis *chassis) {
     knights::RamseteConstants ramsete_constants(1, 0.5);
@@ -17,10 +32,22 @@ void pid_tuning(knights::RobotChassis *chassis) {
 	knights::PIDController lateralPID(LATERAL_kP, LATERAL_kI, LATERAL_kD, 10.0, 127.0);
 	knights::RobotController lateralController(chassis, &lateralPID);
 
-	knights::PIDController turnPID(TURN_kP, TURN_kI, TURN_kD, 10.0, 127.0);
-	knights::RobotController turnController(chassis, &turnPID);
+	knights::PIDController turnPID_45(TURN_kP_45, TURN_kI_45, TURN_kD_45, 10.0, 127.0);
+	knights::RobotController turnController_45(chassis, &turnPID_45);
 
-	lateralController.lateral_move(24);
+	knights::PIDController turnPID_90(TURN_kP_90, TURN_kI_90, TURN_kD_90, 10.0, 127.0);
+	knights::RobotController turnController_90(chassis, &turnPID_90);
+
+	knights::PIDController turnPID_135(TURN_kP_135, TURN_kI_135, TURN_kD_135, 10.0, 127.0);
+	knights::RobotController turnController_135(chassis, &turnPID_135);
+
+	knights::PIDController turnPID_180(TURN_kP_180, TURN_kI_180, TURN_kD_180, 10.0, 127.0);
+	knights::RobotController turnController_180(chassis, &turnPID_180);
+
+	// lateralController.lateral_move(24);
+	turnController_90.turn_to_angle(270, 0);
+
+	turnController_180.turn_to_angle(90, 0);
 }
 
 
@@ -40,8 +67,17 @@ void pp_test(knights::RobotChassis *chassis) {
 	knights::PIDController lateralPID(LATERAL_kP, LATERAL_kI, LATERAL_kD, 10.0, 100.0);
 	knights::RobotController lateralController(chassis, &lateralPID);
 
-	knights::PIDController turnPID(TURN_kP, TURN_kI, TURN_kD, 10.0, 127.0);
-	knights::RobotController turnController(chassis, &turnPID);
+	knights::PIDController turnPID_45(TURN_kP_45, TURN_kI_45, TURN_kD_45, 10.0, 127.0);
+	knights::RobotController turnController_45(chassis, &turnPID_45);
+
+	knights::PIDController turnPID_90(TURN_kP_90, TURN_kI_90, TURN_kD_90, 10.0, 127.0);
+	knights::RobotController turnController_90(chassis, &turnPID_90);
+
+	knights::PIDController turnPID_135(TURN_kP_135, TURN_kI_135, TURN_kD_135, 10.0, 127.0);
+	knights::RobotController turnController_135(chassis, &turnPID_135);
+
+	knights::PIDController turnPID_180(TURN_kP_180, TURN_kI_180, TURN_kD_180, 10.0, 127.0);
+	knights::RobotController turnController_180(chassis, &turnPID_180);
 	
 	knights::input::AutonomousInputMap inputMap;
     inputMap.bind_action("intakeRev", intake_out);
@@ -49,7 +85,7 @@ void pp_test(knights::RobotChassis *chassis) {
     inputMap.bind_action("clamp", clamp_toggle);
 
 
-	test_route.execute(chassis, &lateralPID, &turnPID, &inputMap);
+	test_route.execute(chassis, &lateralPID, &turnPID_90, &inputMap);
 }
 
 #define WAIT 140
@@ -60,9 +96,18 @@ void skills(knights::RobotChassis *chassis) {
 	knights::PIDController lateralPID(LATERAL_kP, LATERAL_kI, LATERAL_kD, 10.0, 127.0);
 	knights::RobotController lateralController(chassis, &lateralPID);
 
-	knights::PIDController turnPID(TURN_kP, TURN_kI, TURN_kD, 10.0, 127.0);
-	knights::RobotController turnController(chassis, &turnPID);
-	
+	knights::PIDController turnPID_45(TURN_kP_45, TURN_kI_45, TURN_kD_45, 10.0, 127.0);
+	knights::RobotController turnController_45(chassis, &turnPID_45);
+
+	knights::PIDController turnPID_90(TURN_kP_90, TURN_kI_90, TURN_kD_90, 10.0, 127.0);
+	knights::RobotController turnController_90(chassis, &turnPID_90);
+
+	knights::PIDController turnPID_135(TURN_kP_135, TURN_kI_135, TURN_kD_135, 10.0, 127.0);
+	knights::RobotController turnController_135(chassis, &turnPID_135);
+
+	knights::PIDController turnPID_180(TURN_kP_180, TURN_kI_180, TURN_kD_180, 10.0, 127.0);
+	knights::RobotController turnController_180(chassis, &turnPID_180);
+
 	intake.set_reversed(false,0);
 	intake.set_reversed(true, 1);
 
@@ -74,8 +119,17 @@ void red_left_wp(knights::RobotChassis *chassis) {
 	knights::PIDController lateralPID(LATERAL_kP, LATERAL_kI, LATERAL_kD, 10.0, 127.0);
 	knights::RobotController lateralController(chassis, &lateralPID);
 
-	knights::PIDController turnPID(TURN_kP, TURN_kI, TURN_kD, 10.0, 127.0);
-	knights::RobotController turnController(chassis, &turnPID);
+	knights::PIDController turnPID_45(TURN_kP_45, TURN_kI_45, TURN_kD_45, 10.0, 127.0);
+	knights::RobotController turnController_45(chassis, &turnPID_45);
+
+	knights::PIDController turnPID_90(TURN_kP_90, TURN_kI_90, TURN_kD_90, 10.0, 127.0);
+	knights::RobotController turnController_90(chassis, &turnPID_90);
+
+	knights::PIDController turnPID_135(TURN_kP_135, TURN_kI_135, TURN_kD_135, 10.0, 127.0);
+	knights::RobotController turnController_135(chassis, &turnPID_135);
+
+	knights::PIDController turnPID_180(TURN_kP_180, TURN_kI_180, TURN_kD_180, 10.0, 127.0);
+	knights::RobotController turnController_180(chassis, &turnPID_180);
 	
 	intake.set_reversed(false,0);
 	intake.set_reversed(true, 1);
@@ -89,8 +143,17 @@ void red_rush_right_wp(knights::RobotChassis *chassis) {
 	knights::PIDController lateralPID(LATERAL_kP, LATERAL_kI, LATERAL_kD, 10.0, 127.0);
 	knights::RobotController lateralController(chassis, &lateralPID);
 
-	knights::PIDController turnPID(TURN_kP, TURN_kI, TURN_kD, 10.0, 127.0);
-	knights::RobotController turnController(chassis, &turnPID);
+	knights::PIDController turnPID_45(TURN_kP_45, TURN_kI_45, TURN_kD_45, 10.0, 127.0);
+	knights::RobotController turnController_45(chassis, &turnPID_45);
+
+	knights::PIDController turnPID_90(TURN_kP_90, TURN_kI_90, TURN_kD_90, 10.0, 127.0);
+	knights::RobotController turnController_90(chassis, &turnPID_90);
+
+	knights::PIDController turnPID_135(TURN_kP_135, TURN_kI_135, TURN_kD_135, 10.0, 127.0);
+	knights::RobotController turnController_135(chassis, &turnPID_135);
+
+	knights::PIDController turnPID_180(TURN_kP_180, TURN_kI_180, TURN_kD_180, 10.0, 127.0);
+	knights::RobotController turnController_180(chassis, &turnPID_180);
     
 	intake.set_reversed(false,0);
 	intake.set_reversed(true, 1);
@@ -103,8 +166,17 @@ void red_right_elim(knights::RobotChassis *chassis) {
 	knights::PIDController lateralPID(LATERAL_kP, LATERAL_kI, LATERAL_kD, 10.0, 127.0);
 	knights::RobotController lateralController(chassis, &lateralPID);
 
-	knights::PIDController turnPID(TURN_kP, TURN_kI, TURN_kD, 10.0, 127.0);
-	knights::RobotController turnController(chassis, &turnPID);
+	knights::PIDController turnPID_45(TURN_kP_45, TURN_kI_45, TURN_kD_45, 10.0, 127.0);
+	knights::RobotController turnController_45(chassis, &turnPID_45);
+
+	knights::PIDController turnPID_90(TURN_kP_90, TURN_kI_90, TURN_kD_90, 10.0, 127.0);
+	knights::RobotController turnController_90(chassis, &turnPID_90);
+
+	knights::PIDController turnPID_135(TURN_kP_135, TURN_kI_135, TURN_kD_135, 10.0, 127.0);
+	knights::RobotController turnController_135(chassis, &turnPID_135);
+
+	knights::PIDController turnPID_180(TURN_kP_180, TURN_kI_180, TURN_kD_180, 10.0, 127.0);
+	knights::RobotController turnController_180(chassis, &turnPID_180);
     
 	intake.set_reversed(false,0);
 	intake.set_reversed(true, 1);
@@ -118,8 +190,17 @@ void red_rush_right_elim(knights::RobotChassis *chassis) {
 	knights::PIDController lateralPID(LATERAL_kP, LATERAL_kI, LATERAL_kD, 10.0, 127.0);
 	knights::RobotController lateralController(chassis, &lateralPID);
 
-	knights::PIDController turnPID(TURN_kP, TURN_kI, TURN_kD, 10.0, 127.0);
-	knights::RobotController turnController(chassis, &turnPID);
+	knights::PIDController turnPID_45(TURN_kP_45, TURN_kI_45, TURN_kD_45, 10.0, 127.0);
+	knights::RobotController turnController_45(chassis, &turnPID_45);
+
+	knights::PIDController turnPID_90(TURN_kP_90, TURN_kI_90, TURN_kD_90, 10.0, 127.0);
+	knights::RobotController turnController_90(chassis, &turnPID_90);
+
+	knights::PIDController turnPID_135(TURN_kP_135, TURN_kI_135, TURN_kD_135, 10.0, 127.0);
+	knights::RobotController turnController_135(chassis, &turnPID_135);
+
+	knights::PIDController turnPID_180(TURN_kP_180, TURN_kI_180, TURN_kD_180, 10.0, 127.0);
+	knights::RobotController turnController_180(chassis, &turnPID_180);
     
 	intake.set_reversed(false,0);
 	intake.set_reversed(true, 1);
@@ -133,8 +214,17 @@ void blue_right_wp(knights::RobotChassis *chassis) {
 	knights::PIDController lateralPID(LATERAL_kP, LATERAL_kI, LATERAL_kD, 10.0, 127.0);
 	knights::RobotController lateralController(chassis, &lateralPID);
 
-	knights::PIDController turnPID(TURN_kP, TURN_kI, TURN_kD, 10.0, 127.0);
-	knights::RobotController turnController(chassis, &turnPID);
+	knights::PIDController turnPID_45(TURN_kP_45, TURN_kI_45, TURN_kD_45, 10.0, 127.0);
+	knights::RobotController turnController_45(chassis, &turnPID_45);
+
+	knights::PIDController turnPID_90(TURN_kP_90, TURN_kI_90, TURN_kD_90, 10.0, 127.0);
+	knights::RobotController turnController_90(chassis, &turnPID_90);
+
+	knights::PIDController turnPID_135(TURN_kP_135, TURN_kI_135, TURN_kD_135, 10.0, 127.0);
+	knights::RobotController turnController_135(chassis, &turnPID_135);
+
+	knights::PIDController turnPID_180(TURN_kP_180, TURN_kI_180, TURN_kD_180, 10.0, 127.0);
+	knights::RobotController turnController_180(chassis, &turnPID_180);
 	
 	intake.set_reversed(false,0);
 	intake.set_reversed(true, 1);
@@ -148,8 +238,17 @@ void blue_rush_left_wp(knights::RobotChassis *chassis) {
 	knights::PIDController lateralPID(LATERAL_kP, LATERAL_kI, LATERAL_kD, 10.0, 127.0);
 	knights::RobotController lateralController(chassis, &lateralPID);
 
-	knights::PIDController turnPID(TURN_kP, TURN_kI, TURN_kD, 10.0, 127.0);
-	knights::RobotController turnController(chassis, &turnPID);
+	knights::PIDController turnPID_45(TURN_kP_45, TURN_kI_45, TURN_kD_45, 10.0, 127.0);
+	knights::RobotController turnController_45(chassis, &turnPID_45);
+
+	knights::PIDController turnPID_90(TURN_kP_90, TURN_kI_90, TURN_kD_90, 10.0, 127.0);
+	knights::RobotController turnController_90(chassis, &turnPID_90);
+
+	knights::PIDController turnPID_135(TURN_kP_135, TURN_kI_135, TURN_kD_135, 10.0, 127.0);
+	knights::RobotController turnController_135(chassis, &turnPID_135);
+
+	knights::PIDController turnPID_180(TURN_kP_180, TURN_kI_180, TURN_kD_180, 10.0, 127.0);
+	knights::RobotController turnController_180(chassis, &turnPID_180);
     
 	intake.set_reversed(false,0);
 	intake.set_reversed(true, 1);
@@ -161,8 +260,17 @@ void blue_right_elim(knights::RobotChassis *chassis) {
 	knights::PIDController lateralPID(LATERAL_kP, LATERAL_kI, LATERAL_kD, 10.0, 127.0);
 	knights::RobotController lateralController(chassis, &lateralPID);
 
-	knights::PIDController turnPID(TURN_kP, TURN_kI, TURN_kD, 10.0, 127.0);
-	knights::RobotController turnController(chassis, &turnPID);
+	knights::PIDController turnPID_45(TURN_kP_45, TURN_kI_45, TURN_kD_45, 10.0, 127.0);
+	knights::RobotController turnController_45(chassis, &turnPID_45);
+
+	knights::PIDController turnPID_90(TURN_kP_90, TURN_kI_90, TURN_kD_90, 10.0, 127.0);
+	knights::RobotController turnController_90(chassis, &turnPID_90);
+
+	knights::PIDController turnPID_135(TURN_kP_135, TURN_kI_135, TURN_kD_135, 10.0, 127.0);
+	knights::RobotController turnController_135(chassis, &turnPID_135);
+
+	knights::PIDController turnPID_180(TURN_kP_180, TURN_kI_180, TURN_kD_180, 10.0, 127.0);
+	knights::RobotController turnController_180(chassis, &turnPID_180);
     
 	intake.set_reversed(false,0);
 	intake.set_reversed(true, 1);
@@ -176,8 +284,17 @@ void blue_left_rush_elim(knights::RobotChassis *chassis) {
 	knights::PIDController lateralPID(LATERAL_kP, LATERAL_kI, LATERAL_kD, 10.0, 127.0);
 	knights::RobotController lateralController(chassis, &lateralPID);
 
-	knights::PIDController turnPID(TURN_kP, TURN_kI, TURN_kD, 10.0, 127.0);
-	knights::RobotController turnController(chassis, &turnPID);
+	knights::PIDController turnPID_45(TURN_kP_45, TURN_kI_45, TURN_kD_45, 10.0, 127.0);
+	knights::RobotController turnController_45(chassis, &turnPID_45);
+
+	knights::PIDController turnPID_90(TURN_kP_90, TURN_kI_90, TURN_kD_90, 10.0, 127.0);
+	knights::RobotController turnController_90(chassis, &turnPID_90);
+
+	knights::PIDController turnPID_135(TURN_kP_135, TURN_kI_135, TURN_kD_135, 10.0, 127.0);
+	knights::RobotController turnController_135(chassis, &turnPID_135);
+
+	knights::PIDController turnPID_180(TURN_kP_180, TURN_kI_180, TURN_kD_180, 10.0, 127.0);
+	knights::RobotController turnController_180(chassis, &turnPID_180);
     
 	intake.set_reversed(false,0);
 	intake.set_reversed(true, 1);
