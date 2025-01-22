014bbb43218931b4e8ebf23a31b39ae97a0543e3
NRGKing <nrgninjaking@gmail.com>
2025-01-21 20:44:12 -0500

    motion profiling work

--- tests/mp/generator.py
+++ tests/mp/generator.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-figure, axis = plt.subplots(2, 1)
+figure, axis = plt.subplots(2, 2)
 
 def dist_between(x1, y1, x2, y2):
   return np.hypot(x2-x1, y2-y1)
@@ -9,34 +9,7 @@ def dist_between(x1, y1, x2, y2):
 def lerp(start, end, step):
   return start + (end-start) * step
 
-def get_total_time(max_acceleration, max_velocity, distance, elapsed_time):
-  # Calculate the time it takes to accelerate to max velocity
-  acceleration_dt = max_velocity / max_acceleration
-
-  # If we can't accelerate to max velocity in the given distance, we'll accelerate as much as possible
-  halfway_distance = distance / 2
-  acceleration_distance = 0.5 * max_acceleration * acceleration_dt ** 2
-
-  if (acceleration_distance > halfway_distance):
-    acceleration_dt = np.sqrt(halfway_distance / (0.5 * max_acceleration))
-
-  acceleration_distance = 0.5 * max_acceleration * acceleration_dt ** 2
-
-  # recalculate max velocity based on the time we have to accelerate and decelerate
-  max_velocity = max_acceleration * acceleration_dt
-
-  # we decelerate at the same rate as we accelerate
-  deceleration_dt = acceleration_dt
-
-  # calculate the time that we're at max velocity
-  cruise_distance = distance - 2 * acceleration_distance
-  cruise_dt = cruise_distance / max_velocity
-  deceleration_time = acceleration_dt + cruise_dt
-
-  # check if we're still in the motion profile
-  return(acceleration_dt + cruise_dt + deceleration_dt)
-
-def motion_profile_pos(max_acceleration, max_velocity, distance, elapsed_time):
+def generate_motion_profile(max_acceleration, max_velocity, distance):
   # Calculate the time it takes to accelerate to max velocity
   acceleration_dt = max_velocity / max_acceleration
 
@@ -62,33 +35,50 @@ def motion_profile_pos(max_acceleration, max_velocity, distance, elapsed_time):
 
   # check if we're still in the motion profile
   entire_dt = acceleration_dt + cruise_dt + deceleration_dt
-  if (elapsed_time > entire_dt):
-    return distance
 
-  # if we're accelerating
-  if (elapsed_time < acceleration_dt):
-    # use the kinematic equation for acceleration
-    return 0.5 * max_acceleration * elapsed_time ** 2
+  t = np.linspace(0, entire_dt, 200)
 
-  # if we're cruising
-  elif (elapsed_time < deceleration_time):
-    acceleration_distance = 0.5 * max_acceleration * acceleration_dt ** 2
-    cruise_current_dt = elapsed_time - acceleration_dt
+  dist_arr = []
+  vel_arr = []
 
-    # use the kinematic equation for constant velocity
-    return acceleration_distance + max_velocity * cruise_current_dt
+  for elapsed_time in t:
+    # Distance Calculations
+    if (elapsed_time > entire_dt):
+      dist_arr.append( distance )
 
-  # if we're decelerating
-  else:
-    acceleration_distance = 0.5 * max_acceleration * acceleration_dt ** 2
-    cruise_distance = max_velocity * cruise_dt
-    deceleration_time = elapsed_time - deceleration_time
+    # if we're accelerating
+    elif (elapsed_time < acceleration_dt):
+      # use the kinematic equation for acceleration
+      dist_arr.append( 0.5 * max_acceleration * elapsed_time ** 2 )
 
-    # use the kinematic equations to calculate the instantaneous desired position
-    return acceleration_distance + cruise_distance + max_velocity * deceleration_time - 0.5 * max_acceleration * deceleration_time ** 2
+    # if we're cruising
+    elif (elapsed_time < deceleration_time):
+      acceleration_distance = 0.5 * max_acceleration * acceleration_dt ** 2
+      cruise_current_dt = elapsed_time - acceleration_dt
+
+      # use the kinematic equation for constant velocity
+      dist_arr.append( acceleration_distance + max_velocity * cruise_current_dt )
+
+    # if we're decelerating
+    else:
+      acceleration_distance = 0.5 * max_acceleration * acceleration_dt ** 2
+      cruise_distance = max_velocity * cruise_dt
+
+      # use the kinematic equations to calculate the instantaneous desired position
+      dist_arr.append( acceleration_distance + cruise_distance + max_velocity * (elapsed_time - deceleration_time) - 0.5 * max_acceleration * (elapsed_time - deceleration_time) ** 2)
+    
+    # Velocity Calculations
+    if elapsed_time < acceleration_dt:
+      vel_arr.append(lerp(0, max_velocity, elapsed_time/acceleration_dt))
+    elif elapsed_time > acceleration_dt and elapsed_time < acceleration_dt + cruise_dt:
+      vel_arr.append(max_velocity)
+    else:
+      vel_arr.append(lerp(max_velocity, 0, (elapsed_time-(acceleration_dt + cruise_dt))/acceleration_dt))
+  
+  return [t, dist_arr, vel_arr]
 
 curr = [0, 0, np.radians(90)]
-target = [4, 4, np.radians(270)]
+target = [24, 24, np.radians(90)]
 
 dist = np.hypot(target[0]-curr[0], target[1]-curr[1]) * 2
 
@@ -117,20 +107,21 @@ MAX_VOLTAGE = 127
 WHEEL_DIAMETER = 2.75
 RPM = 450
 
-max_acceleration = 10 # arbitrary constant
+max_acceleration = 300 # arbitrary constant
 max_velocity = (DESIRED_VOLTAGE/MAX_VOLTAGE) * np.pi * WHEEL_DIAMETER * (RPM / 60.0)
 
-distances = []
-times = np.linspace(0, get_total_time(max_acceleration, max_velocity, total_dist, val), 200)
+print(max_velocity, max_acceleration)
 
-for val in times:
-  distances.append(motion_profile_pos(max_acceleration, max_velocity, total_dist, val))
+result = generate_motion_profile(max_acceleration, max_velocity, total_dist)
 
-axis[0].plot(times, distances)
-axis[0].set_title("Distance Travelled")
+axis[0][1].plot(result[0], result[2])
+axis[0][1].set_title("Velocity")
 
-axis[1].plot(x_val, y_val)
-axis[1].set_title("Path")
+axis[0][0].plot(result[0], result[1])
+axis[0][0].set_title("Distance Travelled")
+
+axis[1][0].plot(x_val, y_val)
+axis[1][0].set_title("Path")
 
 plt.show()
 
