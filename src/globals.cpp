138c9d9e32d6572610a47efa4cfe5577dfa7cc85
Student <cwbys8568@gmail.com>
2025-02-14 21:25:45 -0500

    odom update

--- src/globals.cpp
+++ src/globals.cpp
@@ -30,7 +30,7 @@ pros::Rotation back_odom(	11); // perpendicular tracking
 pros::IMU imu(6);
 //dimensions and positions of odom pods for calculations for position tracking
 knights::PositionTracker midOdom(&mid_odom, 2, 1, 0.9375, -1);
-knights::PositionTracker backOdom(&back_odom, 2, 1, 1.875, -1);
+knights::PositionTracker backOdom(&back_odom, 2, 1, 1.75, -1); // 1.875
 // #### END
 
 // // #### Test Robot
